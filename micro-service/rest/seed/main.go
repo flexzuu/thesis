7c5ff037c90c302899f179d78b75c687854be32f
Jonas Faber <jonas@graphcms.com>
2019-03-04 12:59:57 +0100

    Finish rest seed

--- micro-service/rest/seed/main.go
+++ micro-service/rest/seed/main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"math/rand"
 	"os"
 
-	"github.com/flexzuu/benchmark/micro-service/grpc/post/post"
-	"github.com/flexzuu/benchmark/micro-service/grpc/rating/rating"
-	"github.com/flexzuu/benchmark/micro-service/grpc/user/user"
-	"google.golang.org/grpc"
+	post "github.com/flexzuu/benchmark/micro-service/rest/post/openapi"
+	postApi "github.com/flexzuu/benchmark/micro-service/rest/post/openapi/client"
+	rating "github.com/flexzuu/benchmark/micro-service/rest/rating/openapi"
+	ratingApi "github.com/flexzuu/benchmark/micro-service/rest/rating/openapi/client"
+	user "github.com/flexzuu/benchmark/micro-service/rest/user/openapi"
+	userApi "github.com/flexzuu/benchmark/micro-service/rest/user/openapi/client"
 )
 
 // seed the services with some test data
@@ -19,53 +22,44 @@ func main() {
 	if postServiceAddress == "" {
 		log.Fatalln("please provide POST_SERVICE as env var")
 	}
+	postCfg := postApi.NewConfiguration()
+	postCfg.BasePath = fmt.Sprintf("http://%s", postServiceAddress)
+	postClient := postApi.NewAPIClient(postCfg)
+
 	userServiceAddress := os.Getenv("USER_SERVICE")
 	if userServiceAddress == "" {
 		log.Fatalln("please provide USER_SERVICE as env var")
 	}
+	userCfg := userApi.NewConfiguration()
+	userCfg.BasePath = fmt.Sprintf("http://%s", userServiceAddress)
+	userClient := userApi.NewAPIClient(userCfg)
+
 	ratingServiceAddress := os.Getenv("RATING_SERVICE")
 	if ratingServiceAddress == "" {
 		log.Fatalln("please provide RATING_SERVICE as env var")
 	}
+	ratingCfg := ratingApi.NewConfiguration()
+	ratingCfg.BasePath = fmt.Sprintf("http://%s", ratingServiceAddress)
+	ratingClient := ratingApi.NewAPIClient(ratingCfg)
 
-	postConn, err := grpc.Dial(postServiceAddress, grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("did not connect to post service: %v", err)
-	}
-	defer postConn.Close()
-	postClient := post.NewPostServiceClient(postConn)
-
-	userConn, err := grpc.Dial(userServiceAddress, grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("did not connect to post service: %v", err)
-	}
-	defer userConn.Close()
-	userClient := user.NewUserServiceClient(userConn)
-
-	ratingConn, err := grpc.Dial(ratingServiceAddress, grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("did not connect to post service: %v", err)
-	}
-	defer ratingConn.Close()
-	ratingClient := rating.NewRatingServiceClient(ratingConn)
 	ctx := context.Background()
 
 	//createTestUser
-	john, _ := userClient.Create(ctx, &user.CreateUserRequest{
+	john, _, _ := userClient.UserApi.CreateUser(ctx, user.CreateUserModel{
 		Email: "john@example.com",
 		Name:  "John Doe",
 	})
 	//createTestUser
-	jane, _ := userClient.Create(ctx, &user.CreateUserRequest{
+	jane, _, _ := userClient.UserApi.CreateUser(ctx, user.CreateUserModel{
 		Email: "jane@example.com",
 		Name:  "Jane Doe",
 	})
 
 	// create fake post
-	var createPost = func(author *user.User, headline string) *post.Post {
+	var createPost = func(author user.UserModel, headline string) post.PostModel {
 
-		post, _ := postClient.Create(ctx, &post.CreatePostRequest{
-			AuthorID: author.ID,
+		post, _, _ := postClient.PostApi.CreatePost(ctx, post.CreatePostModel{
+			AuthorId: author.Id,
 			Headline: headline,
 			Content: `Markdown is a lightweight markup language based on the formatting conventions that people naturally use in email.  As [John Gruber] writes on the [Markdown site][df1]
 	
@@ -87,8 +81,8 @@ func main() {
 
 		i := 1
 		for i <= 5 /*num of fake reviews*/ {
-			ratingClient.Create(ctx, &rating.CreateRatingRequest{
-				PostID: postID,
+			ratingClient.RatingApi.CreateRating(ctx, rating.CreateRatingModel{
+				PostId: postID,
 				Rating: rand.Int31n(5),
 			})
 			i++
@@ -102,11 +96,11 @@ func main() {
 	bestBlogPost2 := createPost(jane, "Best Blog Post 2")
 	bestBlogPost3 := createPost(jane, "Best Blog Post 3")
 
-	rate(worstBlogPost1.ID)
-	rate(worstBlogPost2.ID)
-	rate(worstBlogPost3.ID)
-	rate(bestBlogPost1.ID)
-	rate(bestBlogPost2.ID)
-	rate(bestBlogPost3.ID)
+	rate(worstBlogPost1.Id)
+	rate(worstBlogPost2.Id)
+	rate(worstBlogPost3.Id)
+	rate(bestBlogPost1.Id)
+	rate(bestBlogPost2.Id)
+	rate(bestBlogPost3.Id)
 
 }
