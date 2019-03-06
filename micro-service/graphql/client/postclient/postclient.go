74b08362d157eeb9e7213f734d20d5fa4be749d7
Jonas Faber <jonas@graphcms.com>
2019-03-06 12:40:32 +0100

    Finish Client

---
+++ micro-service/graphql/client/postclient/postclient.go
@@ -0,0 +1,89 @@
+package postclient
+
+import (
+	"context"
+
+	"github.com/flexzuu/benchmark/micro-service/graphql/post/repo/entity"
+	"github.com/flexzuu/graphqlt"
+)
+
+type Client struct {
+	*graphqlt.Client
+}
+
+// Client defines what and how to fetch
+func (c *Client) PostGet(ctx context.Context, id int) (*entity.Post, error) {
+	req := graphqlt.NewRequest(`
+	query postGet($id: ID!) {
+		post: postGet(id: $id) {
+			id
+			authorId
+			headline
+			content
+		}
+	}
+	`)
+
+	// set any variables
+	req.Var("id", id)
+
+	// run it and capture the response
+	var respData struct {
+		Data struct {
+			Post entity.Post
+		}
+	}
+	if err := c.Run(ctx, req, &respData); err != nil {
+		return nil, err
+	}
+	return &respData.Data.Post, nil
+}
+func (c *Client) PostList(ctx context.Context) ([]entity.Post, error) {
+	req := graphqlt.NewRequest(`
+	query postList {
+		posts: postList {
+		  id
+		  authorId
+		  headline
+		  content
+		}
+	  }
+	`)
+
+	// run it and capture the response
+	var respData struct {
+		Data struct {
+			Posts []entity.Post
+		}
+	}
+	if err := c.Run(ctx, req, &respData); err != nil {
+		return nil, err
+	}
+	return respData.Data.Posts, nil
+}
+func (c *Client) PostListOfAuthor(ctx context.Context, authorId int) ([]entity.Post, error) {
+	req := graphqlt.NewRequest(`
+	query postListOfAuthor($authorId: ID!) {
+		posts: postListOfAuthor(authorId:$authorId) {
+		  id
+		  authorId
+		  headline
+		  content
+		}
+	  }
+	`)
+
+	// set any variables
+	req.Var("authorId", authorId)
+
+	// run it and capture the response
+	var respData struct {
+		Data struct {
+			Posts []entity.Post
+		}
+	}
+	if err := c.Run(ctx, req, &respData); err != nil {
+		return nil, err
+	}
+	return respData.Data.Posts, nil
+}
